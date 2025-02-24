906540fdae8ff16188754e0c1b5fb1114963a27a
Yaman Mahtha <yamanmahtha@gmail.com>
2025-02-24 19:37:05 +0530

    [Yaman] Project Created

---
+++ src/main/java/com/example/employeepayroll/EmployeepayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayrollApplication.class, args);
+	}
+
+}
